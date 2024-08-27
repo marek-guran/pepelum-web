1b55141613f151cab339e826a56deebc43d5e0d7
Marek Guráň <skemgi2@gmail.com>
2024-08-27 16:26:46 +0200

    update title

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pepelum</title>
+    <title>Pepelum - Comprehensive Guide to Pepecoin (PEP): Buy, Store, Sell, and More</title>
     <link rel="stylesheet" href="libs/bootstrap/bootstrap.min.css">
     <link rel="stylesheet" href="libs/fontawesome/css/all.css">
 
